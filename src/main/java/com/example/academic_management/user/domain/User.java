30d7df626f7f3c1023b4908cf4dc1264985ab14d
seongmin <0411tjdals34@gmail.com>
2025-02-27 03:03:43 +0900

    feat: User 클래스 생성

--- src/main/java/com/example/academic_management/user/domain/User.java
+++ src/main/java/com/example/academic_management/user/domain/User.java
@@ -1,4 +1,43 @@
 package com.example.academic_management.user.domain;
 
 public class User {
+    private final Long id;
+    private final String StudentId;
+    private final String name;
+    private final int grade;
+    private final int gradeScore;
+    private final int departmentScore;
+
+    public User(Long id, String StudentId, String name, int grade, int gradeScore, int departmentScore) {
+        this.id = id;
+        this.StudentId = StudentId;
+        this.name = name;
+        this.grade = grade;
+        this.gradeScore = gradeScore;
+        this.departmentScore = departmentScore;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStudentId() {
+        return StudentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getGradeScore() {
+        return gradeScore;
+    }
+
+    public int getDepartmentScore() {
+        return departmentScore;
+    }
 }
